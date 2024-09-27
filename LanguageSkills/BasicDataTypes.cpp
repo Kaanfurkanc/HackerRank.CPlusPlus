4897d692d87bef726522c8d75de6823038536878
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2024-09-27 10:23:47 +0300

    Data Types

---
+++ LanguageSkills/BasicDataTypes.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int num;
+    long lNum;
+    char ch;
+    float fl;
+    double d;
+    
+    cin >> num >> lNum >> ch >> fl >> d;
+    
+    cout << num << endl;
+    cout << lNum << endl;
+    cout << ch << endl;
+    cout << fixed << fl << endl;
+    cout << fixed << d << endl;
+    return 0;
+}
