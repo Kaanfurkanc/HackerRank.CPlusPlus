d8f7122cf65edbd0105bec65ab126440de6ab562
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2024-11-03 22:34:51 +0300

    StringStream

---
+++ LanguageSkills/StringStream.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    vector<int> numbers;
+	stringstream ss(str);
+    char ch;
+    int number;
+    
+    while (ss >> number){
+        
+        numbers.push_back(number);
+        ss>>ch;
+    }
+    return numbers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
