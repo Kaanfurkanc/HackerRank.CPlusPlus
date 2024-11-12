6d3a9790020032f997fad1650509ccecbedac04e
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2024-11-12 17:03:45 +0300

    arrays

---
+++ LanguageSkills/Arrays.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    
+    int n = 0;
+    cin>>n;
+    int numbers[n];
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin>> numbers[i];
+    }
+    for (int i = n-1; i >= 0; i--)
+    {
+        cout<< numbers[i] << " ";
+    }
+    return 0;
+}
